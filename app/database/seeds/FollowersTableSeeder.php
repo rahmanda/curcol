f4712f8290ecbf212e28e4bc7845ad1fb64ff642
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2015-05-21 14:51:11 +0700

    fix seeder

--- app/database/seeds/FollowersTableSeeder.php
+++ app/database/seeds/FollowersTableSeeder.php
@@ -5,23 +5,15 @@ class FollowersTableSeeder extends Seeder {
   public function run() {
     DB::table('followers')->delete();
 
-    // for($i = 1; $i <= 5; $i++) {
+    for($i = 1; $i <= 5; $i++) {
       $follower = new Follower(array(
-        'id' => 1,
-        'followed_id' => 1
+        'id' => $i,
+        'followed_id' => $i
         ));
 
       $follower->timestamps = false;
       $follower->save();
-
-      $follower = new Follower(array(
-        'id' => 4,
-        'followed_id' => 4
-        ));
-
-      $follower->timestamps = false;
-      $follower->save();
-    // }
+    }
   }
 
 }
