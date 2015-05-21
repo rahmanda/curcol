73d8008ad7595634fea2b61e57bc8a9721a9e92b
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2015-05-21 23:08:50 +0700

    add references for javascript

--- app/views/templates/header.blade.php
+++ app/views/templates/header.blade.php
@@ -2,15 +2,18 @@
   <div>
   <h1 class="brand"><a href="{{ route('timeline') }}">Curcol</a></h1>
   <ul class="list-nav">
-    <li class="user-fullname">
+    <li class="profile-menu hidden">
       @if(Session::has(User::$sessionField['fullname']))
-      <a href="{{ route('profile', Session::get(User::$sessionField['username']))}}">
-        {{ Session::get(User::$sessionField['fullname']) }}
-      </a>
+      {{ Session::get(User::$sessionField['fullname']) }}
       @endif
+      <ul class="profile-sub-menu">
+        <li class="menu-profile"><a href="{{ route('profile', Session::get(User::$sessionField['username'])) }}">Profile</a></li>
+        <li class="menu-setting"><a href="{{ route('setting') }}">Account setting</a></li>
+        <li class="menu-logout"><a href="{{ route('logout') }}">Logout</a></li>
+      </ul>
     </li>
-    <li class="logout">
-      <a href="{{ route('logout') }}">logout</a>
+    <li class="post-menu">
+      <a href="#">Post</a>
     </li>
   </ul>
   <div class="form-search">
