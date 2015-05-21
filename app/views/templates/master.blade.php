90c1aa9135ac55108c2765942ab28d8fb6b2140d
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2015-05-21 23:09:12 +0700

    include timeline template

--- app/views/templates/master.blade.php
+++ app/views/templates/master.blade.php
@@ -2,13 +2,17 @@
 <head>
   <meta charset="utf-8">
   <title>@yield('title')@if(Route::currentRouteName() != 'home') {{ ' - Latweet' }} @endif</title>
-  <link rel="stylesheet" type="text/css" href="{{ URL::asset('style.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ URL::asset('flexboxgrid.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ URL::asset('style/style.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ URL::asset('style/flexboxgrid.min.css') }}">
 </head>
 <body>
   @if(Route::currentRouteName() != 'home' && Route::currentRouteName() != 'register')
-    @include('templates/header')
+    @include('templates.header')
   @endif
   @yield('content')
+  @if(in_array(Route::currentRouteName(), array('search', 'timeline', 'profile')))
+  @include('templates.modal')
+  @endif
+  <script src="{{ URL::asset('js/main.js') }}" type="text/javascript" async></script>
 </body>
 </html>
